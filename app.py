e65e2802d42a6d94ce13fc1034a9bbeb24eb464d
Gary Tse <tse.rong.gary@gmail.com>
2021-12-16 21:54:47 +0800

    feat: sign and recover nonce

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ from datetime import datetime
 
 from dotenv import load_dotenv
 
+from eth_account import (
+    Account,
+)
+
+from eth_account.messages import (
+    encode_defunct
+)
+
 from flask import (
     Flask,
     jsonify,
@@ -13,125 +21,217 @@ from flask import (
     render_template,
 )
 
+from flask_sqlalchemy import SQLAlchemy
+
 from web3 import Web3
 
 from utils import is_transaction_valid
 
+from models import (
+    random_nonce,
+    setup_db,
+    User,
+)
+
 load_dotenv()
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    db = SQLAlchemy()
 
-INFURA_PROJECT_ID = os.getenv('INFURA_PROJECT_ID')
-INFURA_SECRET = os.getenv('INFURA_SECRET')
+    setup_db(app)
 
-UNISWAP_V3_ROUTER_ADDRESS = os.getenv('UNISWAP_V3_ROUTER_ADDRESS')
-UNISWAP_V3_GRAPH_API_URL = os.getenv('UNISWAP_V3_GRAPH_API_URL')
+    INFURA_PROJECT_ID = os.getenv('INFURA_PROJECT_ID')
+    INFURA_SECRET = os.getenv('INFURA_SECRET')
 
-with open("contracts/ERC20ABI.json") as f:
-    erc20_json = json.load(f)
+    UNISWAP_V3_ROUTER_ADDRESS = os.getenv('UNISWAP_V3_ROUTER_ADDRESS')
+    UNISWAP_V3_GRAPH_API_URL = os.getenv('UNISWAP_V3_GRAPH_API_URL')
 
-ERC20ABI = erc20_json
+    w3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{INFURA_PROJECT_ID}'))
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('pages/index.html')
+    with open("contracts/ERC20ABI.json") as f:
+        erc20_json = json.load(f)
 
-@app.route('/trade/add', methods=['POST'])
-def add_trade():
+    ERC20ABI = erc20_json
 
-    data = request.get_json()
-    tx_hash = data['transaction_hash']
+    @app.route('/', methods=['GET'])
+    def index():
+        return render_template('pages/index.html')
 
-    if not is_transaction_valid(tx_hash):
-        return jsonify({
-            'success': False,
-            'message': 'Transaction hash is invalid.'
-        })
+    @app.route('/user', methods=['GET', 'POST'])
+    def login():
 
-    try:
+        if request.method == 'GET':
 
-        w3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{INFURA_PROJECT_ID}'))
+            try:
 
-        tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
+                wallet_address = request.args.get('wallet_address')
+                user = User.query.filter(
+                    User.wallet_address==wallet_address
+                ).first()
 
-        if tx_receipt.to.lower() != UNISWAP_V3_ROUTER_ADDRESS.lower():
+                if not user:
+                    user = User(
+                        wallet_address=wallet_address
+                    )
+                    user.insert()
+
+                return jsonify({
+                    'success': True,
+                    'nonce': user.nonce
+                })
+
+            except Exception as e:
+
+                print(e)
+
+                return jsonify({
+                    'success': False,
+                })
+
+        elif request.method == 'POST':
+
+            try:
+
+                signed_message = request.args.get('msg')
+
+                wallet_address = request.args.get('wallet_address')
+                user = User.query.filter(
+                    User.wallet_address==wallet_address
+                ).first()
+
+                nonce = user.nonce
+                message = encode_defunct(text=nonce)
+
+
+                recovered_address = Account.recover_message(message, signature=signed_message)
+
+                if wallet_address.lower() != recovered_address.lower():
+
+                    return jsonify({
+                        'success': False,
+                        'message': 'We are unable to verify the signed message.'
+                    })
+
+                user.nonce = random_nonce()
+                user.update()
+
+                print("Old nonce: " + str(nonce))
+                print("New nonce: " + str(user.nonce))
+
+                return jsonify({
+                    'success': True
+                })
+                
+            except Exception as e:
+
+                print(e)
+
+                return jsonify({
+                    'success': False
+                })
 
             return jsonify({
-                'success': False,
-                'message': 'The transaction is not a Uniswap V3 transaction.'
+                'success': False
             })
 
-        to_token_log = tx_receipt.logs[0]
-        to_token_address = to_token_log.address
-        to_token_amount = int(to_token_log.data, 16)
-        to_token_contract = w3.eth.contract(
-            address=to_token_address,
-            abi=ERC20ABI
-        )
-        to_token_symbol = to_token_contract.functions.symbol().call()
 
-        from_token_log = tx_receipt.logs[1]
-        from_token_address = from_token_log.address
-        from_token_amount = int(from_token_log.data, 16)
-        from_token_contract = w3.eth.contract(
-            address=from_token_address,
-            abi=ERC20ABI
-        )
-        from_token_symbol = from_token_contract.functions.symbol().call()
+    @app.route('/trade/add', methods=['POST'])
+    def add_trade():
 
+        data = request.get_json()
+        tx_hash = data['transaction_hash']
 
-        block_number = tx_receipt.blockNumber
-        tx_timestamp = datetime.utcfromtimestamp(
-            int(w3.eth.get_block(block_number).timestamp)
-        ).strftime('%Y-%m-%d %H:%M:%S')
+        if not is_transaction_valid(tx_hash):
+            return jsonify({
+                'success': False,
+                'message': 'Transaction hash is invalid.'
+            })
 
-        trading_pair = from_token_symbol + "-" + to_token_symbol
-        short_ratio = to_token_amount / from_token_amount
-
-        query = '''{
-            token(id: "''' +  str(from_token_address).lower() + '''") {
-                derivedETH
-            }
-        }'''
-
-        current_ratio = 0
         try:
 
-            response = requests.post(
-                url=UNISWAP_V3_GRAPH_API_URL,
-                headers={
-                    "Content-Type": "application/json"
-                },
-                json={"query": query}
+            tx_receipt = w3.eth.get_transaction_receipt(tx_hash)
+
+            if tx_receipt.to.lower() != UNISWAP_V3_ROUTER_ADDRESS.lower():
+
+                return jsonify({
+                    'success': False,
+                    'message': 'The transaction is not a Uniswap V3 transaction.'
+                })
+
+            to_token_log = tx_receipt.logs[0]
+            to_token_address = to_token_log.address
+            to_token_amount = int(to_token_log.data, 16)
+            to_token_contract = w3.eth.contract(
+                address=to_token_address,
+                abi=ERC20ABI
             )
+            to_token_symbol = to_token_contract.functions.symbol().call()
 
-            response_data = response.text
+            from_token_log = tx_receipt.logs[1]
+            from_token_address = from_token_log.address
+            from_token_amount = int(from_token_log.data, 16)
+            from_token_contract = w3.eth.contract(
+                address=from_token_address,
+                abi=ERC20ABI
+            )
+            from_token_symbol = from_token_contract.functions.symbol().call()
 
-            start_idx = response_data.find('"derivedETH":"')
-            end_idx = response_data.find('"', start_idx+14)
 
-            current_ratio = float(response_data[start_idx+14:end_idx])
-            profit_loss = Web3.fromWei(to_token_amount - (from_token_amount * current_ratio), 'ether')
+            block_number = tx_receipt.blockNumber
+            tx_timestamp = datetime.utcfromtimestamp(
+                int(w3.eth.get_block(block_number).timestamp)
+            ).strftime('%Y-%m-%d %H:%M:%S')
 
-        except Exception as e:
-            print(e)
+            trading_pair = from_token_symbol + "-" + to_token_symbol
+            short_ratio = to_token_amount / from_token_amount
+
+            query = '''{
+                token(id: "''' +  str(from_token_address).lower() + '''") {
+                    derivedETH
+                }
+            }'''
+
+            current_ratio = 0
+            try:
+
+                response = requests.post(
+                    url=UNISWAP_V3_GRAPH_API_URL,
+                    headers={
+                        "Content-Type": "application/json"
+                    },
+                    json={"query": query}
+                )
+
+                response_data = response.text
+
+                start_idx = response_data.find('"derivedETH":"')
+                end_idx = response_data.find('"', start_idx+14)
+
+                current_ratio = float(response_data[start_idx+14:end_idx])
+                profit_loss = Web3.fromWei(to_token_amount - (from_token_amount * current_ratio), 'ether')
+
+            except Exception as e:
+                print(e)
+                return jsonify({
+                    'success': False,
+                    'message': 'We encountered an issue while trying to retrieve the transaction. Please try again.'
+                })
+
+            return jsonify({
+                'success': True,
+                'trading_pair': trading_pair,
+                'timestamp': tx_timestamp,
+                'short_ratio': short_ratio,
+                'current_ratio': current_ratio,
+                'profit_loss': profit_loss,
+            })
+
+        except:
+
             return jsonify({
                 'success': False,
                 'message': 'We encountered an issue while trying to retrieve the transaction. Please try again.'
             })
 
-        return jsonify({
-            'success': True,
-            'trading_pair': trading_pair,
-            'timestamp': tx_timestamp,
-            'short_ratio': short_ratio,
-            'current_ratio': current_ratio,
-            'profit_loss': profit_loss,
-        })
-
-    except:
-
-        return jsonify({
-            'success': False,
-            'message': 'We encountered an issue while trying to retrieve the transaction. Please try again.'
-        })
+    return app
--- static/js/connect.js
+++ static/js/connect.js
@@ -3,23 +3,43 @@ const showAccount = document.querySelector('.showAccount');
 const showNetwork = document.querySelector('.showNetwork');
 
 ethereumButton.addEventListener('click', () => {
-  getAccount();
+  login();
 });
 
-async function getAccount() {
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  if (account) {
-	  ethereumButton.disabled = true;
-	  ethereumButton.innerHTML = 'Connected';
-	  getChainId();
-  }
-  showAccount.innerHTML = account;
+const provider = web3.currentProvider;
+myWeb3 = new Web3(provider);
+
+async function login() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    if (account) {
+
+
+        fetch('/user?wallet_address=' + account, {
+    		method: 'GET',
+    	})
+    	.then(response => response.json())
+    	.then(data => {
+    		if (data['success'] === true) {
+
+    			nonce = data['nonce'];
+                signMessage(account, nonce)
+
+    		} else if (data['success'] === false) {
+
+                alert("We encountered an error. Please try again.");
+    		}
+    	})
+    	.catch((error) => {
+    		console.log('Error: ', error);
+    	});
+    }
+
 }
 
 async function getChainId() {
 	const chainId = ethereum.chainId;
-	console.log(chainId);
+
 	if (chainId === '0x1') {
 		showNetwork.innerHTML = 'Mainnet';
 	} else if (chainId === '0x4') {
@@ -27,4 +47,37 @@ async function getChainId() {
 	} else {
 		showNetwork.innerHTML = 'Not Mainnet or Rinkeby!';
 	}
+}
+
+async function signMessage(_account, _nonce) {
+
+    myWeb3.eth.personal.sign(_nonce, _account)
+    .then(signedMessage => {
+
+        fetch('/user?wallet_address=' + _account + "&msg=" + signedMessage, {
+    		method: 'POST',
+    	})
+    	.then(response => response.json())
+    	.then(data => {
+    		if (data['success'] === true) {
+
+                ethereumButton.disabled = true;
+        	    ethereumButton.innerHTML = 'Connected';
+                showAccount.innerHTML = _account;
+
+                getChainId();
+
+    		} else if (data['success'] === false) {
+
+                alert(data['message']);
+    		}
+    	})
+    	.catch((error) => {
+    		console.log('Error: ', error);
+    	})
+
+    })
+    .catch((error) => {
+        alert("We encountered an error. Please try again.");
+    });
 }
