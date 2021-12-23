dc4b7405031e95994ce8e661c9e338fd7942d2c8
Gary Tse <tse.rong.gary@gmail.com>
2021-12-23 09:55:20 +0800

    refactor to single js file

--- static/js/transaction.js
+++ static/js/transaction.js
@@ -1,4 +1,126 @@
 const transactionTable = document.getElementById('transaction-table');
+const error_message_field = document.getElementById('error-message');
+
+const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
+const showNetwork = document.querySelector('.showNetwork');
+
+ethereumButton.addEventListener('click', () => {
+  login();
+});
+
+const provider = web3.currentProvider;
+myWeb3 = new Web3(provider);
+
+async function login() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    if (account) {
+
+
+        fetch('/user?wallet_address=' + account, {
+    		method: 'GET',
+    	})
+    	.then(response => response.json())
+    	.then(data => {
+    		if (data['success'] === true) {
+
+    			nonce = data['nonce'];
+                signMessage(account, nonce)
+
+    		} else if (data['success'] === false) {
+
+                alert("We encountered an error. Please try again.");
+    		}
+    	})
+    	.catch((error) => {
+    		console.log('Error: ', error);
+    	});
+    }
+
+}
+
+async function getChainId() {
+	const chainId = ethereum.chainId;
+
+	if (chainId === '0x1') {
+		showNetwork.innerHTML = 'Mainnet';
+	} else if (chainId === '0x4') {
+		showNetwork.innerHTML = 'Rinkeby';
+	} else {
+		showNetwork.innerHTML = 'Not Mainnet or Rinkeby!';
+	}
+}
+
+async function signMessage(_account, _nonce) {
+
+    myWeb3.eth.personal.sign(_nonce, _account)
+    .then(signedMessage => {
+
+        fetch('/user?wallet_address=' + _account + "&msg=" + signedMessage, {
+    		method: 'POST',
+    	})
+    	.then(response => response.json())
+    	.then(data => {
+    		if (data['success'] === true) {
+
+                ethereumButton.disabled = true;
+        	    ethereumButton.innerHTML = 'Connected';
+                showAccount.innerHTML = _account;
+
+                getChainId();
+                getTransactions();
+
+    		} else if (data['success'] === false) {
+
+                alert(data['message']);
+    		}
+    	})
+    	.catch((error) => {
+    		console.log('Error: ', error);
+    	})
+
+    })
+    .catch((error) => {
+        alert("We encountered an error. Please try again.");
+    });
+}
+
+async function getTransactions() {
+
+    fetch('/user/transactions', {
+        method: 'POST',
+        credentials: 'include',
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data['success'] === true) {
+
+            console.log(data);
+
+			var transactions = data['transactions']
+
+			for (let i=0; i<transactions.length; i++) {
+
+				addTransactionRecordToTable(
+					transactions[i]['trading_pair'],
+					transactions[i]['timestamp'],
+					transactions[i]['short_ratio'],
+					transactions[i]['current_ratio'],
+					transactions[i]['profit_loss']
+				)
+			}
+
+        } else if (data['success'] === false) {
+
+            alert(data['message']);
+        }
+    })
+    .catch((error) => {
+        console.log('Error: ', error);
+    })
+}
+
 
 function addTransaction() {
 	transaction_hash = document.getElementById('transaction-hash').value;
@@ -19,6 +141,8 @@ function addTransaction() {
 	.then(data => {
 		if (data['success'] === true) {
 
+			error_message_field.innerHTML = '';
+
 			addTransactionRecordToTable(
 				data['trading_pair'],
 				data['timestamp'],
@@ -27,7 +151,7 @@ function addTransaction() {
 				data['profit_loss']
 			)
 		} else if (data['success'] === false) {
-			error_message_field = document.getElementById('error-message');
+
 			error_message_field.innerHTML = data['message'];
 		}
 	})
