0ffbfbb2a9f5f87fda152722665addc7150fd82b
Gary Tse <tse.rong.gary@gmail.com>
2021-12-23 09:54:40 +0800

    update transaction model

--- models.py
+++ models.py
@@ -9,6 +9,10 @@ from flask_migrate import Migrate
 
 from random import randint
 
+from utils.transaction import calculate_profit_loss
+from utils.fetchers.uniswap_v3_fetcher import get_uniswap_v3_price
+
+
 load_dotenv()
 
 database_path = os.getenv('DATABASE_URL')
@@ -55,6 +59,30 @@ class User(db.Model):
         db.session.delete(self)
         db.session.commit()
 
+    def get_transactions(self):
+
+        transactions = Transaction.query.filter(
+            Transaction.user_address==self.wallet_address
+        ).all()
+
+        transaction_data = []
+
+
+        for t in transactions:
+            t_data = t.to_dict()
+            current_ratio = get_uniswap_v3_price(
+                t_data['from_token_address']
+            )
+            t_data['current_ratio'] = current_ratio
+            t_data['profit_loss'] = calculate_profit_loss(
+                t_data['to_token_amount'],
+                t_data['from_token_amount'],
+                current_ratio
+            )
+            transaction_data.append(t_data)
+
+        return transaction_data
+
 
 class Transaction(db.Model):
 
@@ -91,9 +119,38 @@ class Transaction(db.Model):
         nullable=False,
     )
 
+    to_token_amount = db.Column(
+        db.Numeric,
+        nullable=False,
+    )
+
+    from_token_amount = db.Column(
+        db.Numeric,
+        nullable=False,
+    )
+
+    from_token_address = db.Column(
+        db.String,
+        nullable=False
+    )
+
     __table_args__ = (
         UniqueConstraint('user_address', 'tx_hash'),
     )
+
+    def to_dict(self):
+
+        data = {
+            'tx_hash': self.tx_hash,
+            'trading_pair': self.trading_pair,
+            'timestamp': self.opened_on,
+            'short_ratio': self.short_ratio,
+            'to_token_amount': self.to_token_amount,
+            'from_token_amount': self.from_token_amount,
+            'from_token_address': self.from_token_address,
+        }
+
+        return data
 
     def insert(self):
         db.session.add(self)
